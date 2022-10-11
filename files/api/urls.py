fc0b07a73d981c4311c0b53df30c1cdd68ac0205
Tajwar <tajwaraashab@gmail.com>
2022-10-11 11:15:54 +0600

    used class based views for file upload

--- files/api/urls.py
+++ files/api/urls.py
@@ -6,4 +6,6 @@ from .import views
 
 urlpatterns = [
     path('upload/', views.fileUpload, name='upload-file'),
+    path('test/', views.GetTest.as_view(), name='get-test'),
+    path('uploadv2/', views.UploadFile.as_view(), name='upload-file-v2')
 ]
--- files/api/views.py
+++ files/api/views.py
@@ -1,9 +1,11 @@
 from random import sample
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import FileUploadParser
 from .serializers import FileSerializer
+
 
 import io
 from minio import Minio
@@ -25,6 +27,47 @@ bucket_name = "djangofilestorage"
 
 
 
+# trying out a class based view
+class GetTest(APIView):
+    def get(self, request, format = None):
+        return Response({"message": "Testing class based view"})
+
+
+# file upload
+class UploadFile(APIView):
+    # @route - post - upload file
+    def post(self, request, format=None):
+        # get file
+        # get the file metadata
+        # store the metadata in mongodb
+        # store the file in minio
+
+
+
+        # get file metadata (file type, file size, file name)
+        # get additional metadata -> user_id, username
+        file = request.FILES.get('file')
+        file_metadata = {
+            'file_size' : file.size,
+            'file_type' : file.content_type,
+            'file_name' : file.name,
+        }
+            # store the data in mongoDB
+        collection = db['fileMetaData']
+        insert = collection.insert_one(file_metadata)
+        file_id = str(insert.inserted_id)
+
+        # now handle file
+        fileBytes = '.'.encode('utf-8')
+        for chunk in file.chunks():
+            fileBytes += chunk
+        fileValueStream = io.BytesIO(fileBytes)
+        
+        minioBucket.put_object("djangofilestorage", file_id, fileValueStream, length=len(fileBytes))
+
+        return Response({"message": "Successfully Uploaded File"})
+
+
 # file upload
 @api_view(['POST'])
 def fileUpload(request):
@@ -35,6 +78,7 @@ def fileUpload(request):
     
 
     # get file metadata (file type, file size, file name)
+    # get additional metadata -> user_id, username
     # the db will take care of the created_at and updated_at metadata    
     file = request.FILES.get("file")
     file_metadata = {
@@ -54,6 +98,7 @@ def fileUpload(request):
     fileValueStream = io.BytesIO(fileBytes)
     
     minioBucket.put_object("djangofilestorage", file_id, fileValueStream, length=len(fileBytes))
+
 
     # store 
 
